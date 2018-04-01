f28b342691c4bf2cb00244b623cdacaf1f9d0423
Pascal COMBES <pascom@orange.fr>
2018-04-01 11:33:56 +0200

    Refactoring, comments and improvements:
      - Exceptions in generative testing function yield a test failure (instead of a warning)
      - Generative testing spec now accepts a generator (and not only an arry of generators)
    as second argument too.

--- jasmine/jasmine-gentest.js
+++ jasmine/jasmine-gentest.js
@@ -1,32 +1,31 @@
+// Wrap generative tester into spec:
 function gentest(it) {
+    // The new spec function (takes two or three arguments):
     return function(/* desc, [types,] fun*/) {
-        var desc = arguments[0];
-        
-        // Call original function:
+        // If two arguments are passed, call original function
         if (arguments.length == 2) {
-            return it(desc, arguments[1]);
+            return it(arguments[0], arguments[1]);
         }
-        
-        // Generated testing:
-        var fun = arguments[2];
-        var genTypes = arguments[1];
-        
+
+        // Basic random number generator:
         var rng = {
             float: function() {
                 return Math.random();
             }
-        }
-        
-        var genSpec = it(desc, genFun);
+        };
+
+        // Saves arguments into variables and create generative testing spec:
+        var fun = arguments[2];
+        var genDesc = arguments[0];
+        var genTypes = arguments[1];
+        var genSpec = it(genDesc, genFun);
         return genSpec;
         
+        // Genetive testing function (the function called by the spec):
         function genFun() {
-            var result;
-            var specAddExpectationResult = genSpec.addExpectationResult.bind(genSpec);
-            genSpec.addExpectationResult = function(passed, data, isError) {
-                result = data;
-            };
-            
+            var result; // TODO there may be a problem here (multiple expectations)
+
+            // Wrapper around fun to comply with generative testing (the function called by generative tester):
             var testFun = function() {
                 try {
                     fun.apply(null, arguments);
@@ -41,17 +40,35 @@ function gentest(it) {
                     };
                     throw e;
                 }
-                
                 return result.passed;
             };
+
+            // Alter spec so that expectations are added only after generative testing is done
+            // and exceptions in this function yield a spec failure
+            var specAddExpectationResult = genSpec.addExpectationResult.bind(genSpec);
+            genSpec.onException = function(e) {
+                specAddExpectationResult(false, {
+                    matcherName: '',
+                    passed: false,
+                    expected: '',
+                    actual: '',
+                    error: e,
+                    message: e.toString() + ' (' + e.fileName + ' at ' + e.lineNumber + ':' + e.columnNumber + ').',
+                }, true);
+            };
+            genSpec.addExpectationResult = function(passed, data, isError) {
+                result = data;
+            };
             
-            var prop = new Property(testFun, desc, types.tuple(genTypes));
+            // The property to test and the current test case:
+            var prop = new Property(testFun, genDesc, Array.isArray(genTypes) ? types.tuple(genTypes) : genTypes);
             var testCase;
-            
+
             // TODO Should be options:
             var maxSize = 100;
             var numTests = 10;
             
+            // Run generative testing tests
             for (k = 0; k < numTests; k++) {
                 testCase = prop.genTest(rng, maxSize * (k + 1) / numTests);
                 prop.runTest(testCase);
@@ -60,14 +77,18 @@ function gentest(it) {
                     break;
             }
             
+            // Shrink failing testcase
             if (result.passed === false) { // TODO Enable/disable shriking by option
-                var iter = prop.shrinkFailingTest(testCase);
-                var lastFailedResult = result;
-                testCase = null;  // Allow GC of unused parts of the tree.
-                
-                var numAttempts = 0;
-                var numShrinks = 0;
+                var iter = prop.shrinkFailingTest(testCase); // Test case tree iterator
+                var lastFailedResult = result;               // Result of last failed expectation
 
+                // GC unused branches of the tree
+                testCase = null;
+                
+                var numAttempts = 0; // Number of tries done   // TODO max attempts in option
+                var numShrinks = 0;  // Number of shrinks done
+
+                // Test case tree shriking
                 var ret = iter.next();
                 while (!ret.done) {
                     var value = ret.value;
@@ -82,9 +103,11 @@ function gentest(it) {
                     ret = iter.next();
                 }
                 
+                // Put last failed expectation in test result
                 result = lastFailedResult;
             }
             
+            // Add test result to spec
             specAddExpectationResult(result.passed, result, result.error === undefined);
         }
     }
@@ -93,6 +116,7 @@ function gentest(it) {
 if (!describe)
     throw new ReferenceError('GenTest wrapper for Jasmine "jasmine-gentest.js" must be loaded after Jasmine itself');
 
+// Wrap generative testing in it, fit and xit:
 if (it)
     it = gentest(it);
 if (fit)
